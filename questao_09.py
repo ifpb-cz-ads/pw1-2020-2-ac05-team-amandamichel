275d7156d903d0e02701de2d8a92e56c4df92425
AmandaSimoes <50212426+Amanda-Simoes@users.noreply.github.com>
2021-03-10 11:01:26 -0300

    Ajuste na questão 09

--- questao_09.py
+++ questao_09.py
@@ -11,8 +11,13 @@ Exemplo: "programacao" -> {"p": 1, "r": 2, "o": 2, "g": 1, "a": 2, "m": 1, "c": 
 palavra = input("Informe uma palavra ou uma frase: ")
 
 dicionario = {}
+i = 0
+pos = 0
 
-for i in range(len(palavra)):
-    dicionario[i] = palavra[i]
+while i < len(palavra):
+    if palavra[i] != " ":
+        dicionario[pos] = palavra[i]
+        pos = pos + 1
+    i = i + 1
 
 print(dicionario)
